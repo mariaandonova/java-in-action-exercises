7f945646dbf2749b49ee45fe0aa642cc394c9e7f
mariap <mariap@maria.local>
2010-11-21 11:42:52 +0200

    Failed commit: Default

---
+++ src/main/java/jia/begins/exercises/basics/maria/porlidalieva/Exersize2.java
@@ -0,0 +1,19 @@
+package jia.begins.exercises.basics.maria.porlidalieva;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mariap
+ * Date: Nov 21, 2010
+ * Time: 11:42:11 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Exersize2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i=0;i<1000;i++){
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
